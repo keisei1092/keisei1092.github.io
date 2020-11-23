8ad27f6a4250e1053138b1d9e18cdc79d90b4b2f
keisei1092 <keisei_1092@me.com>
2020-11-23 12:18:28 +0900

    display japanese flag on japanese holidays

---
+++ _layouts/script.js
@@ -0,0 +1,13 @@
+// ページの一番上に国旗を表示する
+const addFlagToTitle = () => {
+    const japaneseFlag = document.getElementById("japanese-flag");
+    japaneseFlag.style.display = 'block';
+};
+
+const isHoliday = () => {
+    // 暫定で勤労感謝の日かどうかだけ判別する
+    const today = new Date();
+    return today.getMonth() === 10 && getDay() === 23;
+};
+
+if (isHoliday()) addFlagToTitle();
