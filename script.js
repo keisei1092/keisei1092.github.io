b4653991e9c0392af2cd589b9904cdfc9e908bb1
keisei1092 <keisei_1092@me.com>
2021-02-11 11:46:26 +0900

    install prettier

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // ページの一番上に国旗を表示する
 const addFlagToTitle = () => {
-    const japaneseFlag = document.getElementById("japanese-flag");
-    japaneseFlag.style.display = 'block';
+  const japaneseFlag = document.getElementById("japanese-flag");
+  japaneseFlag.style.display = "block";
 };
 
 const isHoliday = () => {
-    // 暫定で元旦 or 勤労感謝の日かどうかだけ判別する
-    const today = new Date();
-    return (today.getMonth() === 10 && today.getDate() === 23) || (today.getMonth() === 0 && today.getDate() === 1);
+  // 暫定で元旦、建国記念の日、勤労感謝の日かどうかだけ判別する
+  const today = new Date();
+  return (
+    (today.getMonth() === 10 && today.getDate() === 23) ||
+    (today.getMonth() === 0 && today.getDate() === 1) ||
+    (today.getMonth() === 1 && today.getDate() === 11)
+  );
 };
 
 if (isHoliday()) addFlagToTitle();
