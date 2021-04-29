e4ff94c5f5c3417b9052006e08c4fa147360732b
keisei1092 <keisei_1092@me.com>
2021-04-29 21:00:38 +0900

    add holidays

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Array.some() をサポートしていないブラウザに対応
 // Production steps of ECMA-262, Edition 5, 15.4.4.17
 // Reference: https://es5.github.io/#x15.4.4.17
 if (!Array.prototype.some) {
@@ -25,7 +26,9 @@ if (!Array.prototype.some) {
   };
 }
 
-// ページの一番上に国旗を表示する
+/** ページの一番上に国旗を表示する
+ *  flag クラスがデフォルトで display: none; になっているのを block に変える
+ */ 
 const addFlagToTitle = () => {
   const japaneseFlag = document.getElementById("japanese-flag");
   japaneseFlag.style.display = "block";
@@ -33,13 +36,17 @@ const addFlagToTitle = () => {
 
 const isHoliday = () => {
   const today = new Date();
-  
+
   // 祝日を追加していく
   const holidays = [
     { month: 0, date: 1 }, // 元日
     { month: 1, date: 11 }, // 建国記念の日
     { month: 1, date: 23 }, // 天皇誕生日
-    { month: 10, date: 23 } // 勤労感謝の日
+    { month: 3, date: 29 }, // 昭和の日
+    { month: 4, date: 3 }, // 憲法記念日
+    { month: 4, date: 4 }, // みどりの日
+    { month: 4, date: 5 }, // こどもの日
+    { month: 10, date: 23 }, // 勤労感謝の日
   ];
 
   return holidays.some(holiday => holiday.month === today.getMonth() && holiday.date === today.getDate());
